f49a42c19e9bc5f8ccde2b7eed071172e57f2f6f
METADIL MÓVEIS - Caio Lesnock <caio@metadil.com.br>
2020-09-09 17:14:29 -0300

    Add Change State

--- src/game/states/GameState.ts
+++ src/game/states/GameState.ts
@@ -18,14 +18,7 @@ class GameState extends State {
   animation!: Animation;
   spritesheet!: SpriteSheet;
 
-  canUpdate = false
-  canRender = false
-
   async start() {
-    setTimeout(() => {
-      this.canUpdate = true
-      this.canRender = true
-    }, 1000)
 
     this.blockX = 100
     this.blockY = 100
@@ -34,40 +27,40 @@ class GameState extends State {
 
     const image = await this.loader.loadImage('game/images/test.jpg')
 
-    this.sprite = new Sprite(image)
-    this.tile = new Tile(image)
+    this.sprite = new Sprite(image, {
+      opacity: .5
+    })
+    // this.tile = new Tile(image)
 
-    const image2 = await this.loader.loadImage('game/images/mario.jpg')
-    const image3 = await this.loader.loadImage('game/images/mario.png')
+    // const image2 = await this.loader.loadImage('game/images/mario.jpg')
+    // const image3 = await this.loader.loadImage('game/images/mario.png')
 
-    this.animation = new Animation([
-      { frame: this.sprite, speed: 500 },
-      { frame: image2, speed: 500 },
-    ], true)
+    // this.animation = new Animation([
+    //   { frame: this.sprite, speed: 500 },
+    //   { frame: image2, speed: 500 },
+    // ], true)
 
-    this.spritesheet = new SpriteSheet(image)
-    this.spritesheet.defineSprite('primeiro', 0, 0, 100, 100)
-    this.spritesheet.defineSprite('segundo', 200, 200, 20, 20)
-    this.spritesheet.defineTile(0, 100, 100, 20, 20)
+    // this.spritesheet = new SpriteSheet(image)
+    // this.spritesheet.defineSprite('primeiro', 0, 0, 100, 100)
+    // this.spritesheet.defineSprite('segundo', 200, 200, 20, 20)
+    // this.spritesheet.defineTile(0, 100, 100, 20, 20)
 
-    this.spritesheet.defineAnimation('test', [
-      {
-        frame: 'primeiro',
-        speed: 500
-      },
-      {
-        frame: 'segundo',
-        speed: 500
-      }
-    ], true)
+    // this.spritesheet.defineAnimation('test', [
+    //   {
+    //     frame: 'primeiro',
+    //     speed: 500
+    //   },
+    //   {
+    //     frame: 'segundo',
+    //     speed: 500
+    //   }
+    // ], true)
 
     // Layer 0 - Background
-    this.layers.add({ isVisible: true })
+    this.layers.add()
 
     // Layer 1 - Foreground
-    this.layers.add({ isVisible: true, transparency: 1 })
-
-    // this.layers.get(0).canRender = false
+    // this.layers.add({ opacity: .5 })
   }
 
   update(dt: number) {
@@ -76,28 +69,29 @@ class GameState extends State {
     }
 
     if (this.input.get('KeyD')) {
-      this.changeToState(new LoadingState(), {
+      this.changeState(new LoadingState(), {
         nextLevel: '2'
       })
     }
   }
 
-  render(g: Graphics) {
+  async render(g: Graphics) {
     // g.drawSprite(this.sprite, 0, 0, 100, 100)
     // g.drawTile(this.tile, 100, 0, 100, 100)
 
     // g.drawAnimation(this.animation, 200, 0, 100, 100)
-
-    this.onLayer(1).drawRect(this.blockX, this.blockY, 100, 100, '#550000')
 
     // this.spritesheet.drawSprite(g, 'primeiro', 500, 0)
     // this.spritesheet.drawTile(g, 0, 400, 0)
     // // this.spritesheet.drawAnimation(g, 'test', 400, 400)
     // g.drawAnimation(<Animation>this.spritesheet.animations.get('test'), 400, 400)
 
-    this.onLayer(0).drawRect(0, 0, 300, 800, '#555500')
-    this.onLayer(0).drawSprite(this.sprite, 0, 0)
-    this.onLayer(1).drawRect(0, 0, 300, 300, '#0000FF')
+    // g.layer()
+
+    this.onLayer(0).drawRect(0, 0, 300, 800, '#550000')
+    this.onLayer(0).drawSprite(this.sprite, 100, 0)
+    // this.onLayer(1).drawRect(0, 0, 300, 300, '#0000FF')
+    // this.onLayer(1).drawRect(this.blockX, this.blockY, 100, 100, '#550000')
 
     // this.onLayer(1).drawRect()
     // this.layers.get(1).g.drawRect
--- src/game/states/LoadingState.ts
+++ src/game/states/LoadingState.ts
@@ -2,24 +2,35 @@ import { State, Graphics } from '2djs'
 import GameState from './GameState'
 
 class LoadingState extends State {
-  canUpdate: boolean = false
+  nextLevel!: string
 
-  async start({ nextLevel }: any) {
-    setTimeout(() => {
-      this.canUpdate = true
-    }, 1000)
+  async start({ nextLevel }: any = {}) {
+    console.log('start Loading')
+    this.nextLevel = nextLevel
+    this.loadAssets()
+  }
+
+  async loadAssets() {
+    const image = await this.loader.loadImage('game/images/test.jpg')
+    const image2 = await this.loader.loadImage('game/images/mario.jpg')
+    const image3 = await this.loader.loadImage('game/images/mario.png')
+
+    await new Promise(resolve => {
+      setTimeout(resolve, 5000)
+    })
+
+    this.changeState(new GameState(), {
+      nextLevel: '2'
+    })
   }
 
   update() {
-    if (this.input.get('KeyD')) {
-      this.changeToState(new GameState(), {
-        nextLevel: '2'
-      })
-    }
+
   }
 
   render(g: Graphics) {
-    g.drawRect(0, 0, 200, 200)
+    g.context.font = '48px sans-serif'
+    g.context.fillText('Loading...', 100, 100)
   }
 }
 
